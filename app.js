1456426443013e463321860227f63d464edbb72d
johnmellis <john@johnmellis.com>
2015-03-27 21:55:53 -0700

    Finished front end - first section

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.factory('posts',[function(){
   };
 
   return o;
-}])
+}]);
 
 app.controller('MainCtrl',[
   '$scope',
@@ -27,7 +27,11 @@ app.controller('MainCtrl',[
       $scope.posts.push({
         title   : $scope.title,
         link    : $scope.link,
-        upvotes : 0
+        upvotes : 0,
+        comments:[
+          {author:'joe', body:'Cool Post!', upvotes:0},
+          {author:'steve', body:'Horrible Idea!',upvotes:0}
+        ]
       });
       $scope.title = '';
       $scope.link = '';
@@ -36,6 +40,25 @@ app.controller('MainCtrl',[
     $scope.incrementUpvotes  = function(post){
       post.upvotes += 1;
     };
+  }]);
+
+app.controller('PostsCtrl',[
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope,$stateParams,posts){
+    $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function(){
+      if($scope.body === '') {return;}
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes:0
+      });
+
+      $scope.body='';
+    }
   }]);
 
 app.config([
@@ -47,6 +70,11 @@ app.config([
         url:'/home',
         templateUrl: '/home.html',
         controller: 'MainCtrl'
+      })
+      .state('posts',{
+        url:'/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
       });
 
     $urlRouterProvider.otherwise('home');
